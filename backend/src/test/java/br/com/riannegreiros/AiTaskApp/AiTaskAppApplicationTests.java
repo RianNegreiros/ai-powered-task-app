11a506869bb18bae93eef4a7fab681e23a2ac739
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2026-02-27 13:22:52 -0300

    refactor: move backend code to its own folder

---
+++ backend/src/test/java/br/com/riannegreiros/AiTaskApp/AiTaskAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.riannegreiros.AiTaskApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiTaskAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
